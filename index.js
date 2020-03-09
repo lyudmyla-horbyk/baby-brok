b582dc014252874a1bcd95b2d208ef30d350fe05
Liudmyla Liashuk <lyudmyla.horbyk@gmail.com>
2020-03-09 18:28:13 +0200

    used one document ready

--- index.js
+++ index.js
@@ -10,39 +10,37 @@ function scrollToTop() {
   }
 }
 $(document).ready(function() {
-  const $slider = $(".slider-inner");
+  const $sliderInner = $(".slider-inner");
   const $dots = $(".slider .slider__bottom__dot");
-  $slider.slick({
+  $sliderInner.slick({
     arrows: false,
     dots: 0,
     initialSlide: 1,
     autoplay: true
   });
-  $slider.on("afterChange", function(event, slick, currentSlide) {
+  $sliderInner.on("afterChange", function(event, slick, currentSlide) {
     $dots.removeClass("active");
     $dots.eq(currentSlide).addClass("active");
   });
   $dots.click(function(e) {
     e.preventDefault();
-    $slider.slick("slickGoTo", $(this).index());
+    $sliderInner.slick("slickGoTo", $(this).index());
     $dots.removeClass("active");
     $(this).addClass("active");
   });
-});
-$(document).ready(function() {
-  const $slider = $(".goods");
+  const $sliderGoods = $(".goods");
   const $buttons = $(
     ".goods-pagination .page-link:not(.left-arrow):not(.right-arrow)"
   );
   const $leftArrow = $(".goods-pagination .left-arrow");
   const $rightArrow = $(".goods-pagination .right-arrow");
-  $slider.slick({
+  $sliderGoods.slick({
     arrows: false,
     dots: false,
     slidesToShow: 4,
     slidesToScroll: 4
   });
-  $slider.on("afterChange", function(event, slick, currentSlide) {
+  $sliderGoods.on("afterChange", function(event, slick, currentSlide) {
     $buttons.removeClass("active");
     console.log("current slide", currentSlide);
     $buttons.eq(Math.round(currentSlide / 4)).addClass("active");
@@ -51,16 +49,16 @@ $(document).ready(function() {
     console.log($($buttons).index(this));
     console.log($($buttons).index(this) * 4 + 1);
     e.preventDefault();
-    $slider.slick("slickGoTo", $($buttons).index(this) * 4);
+    $sliderGoods.slick("slickGoTo", $($buttons).index(this) * 4);
     $buttons.removeClass("active");
     $(this).addClass("active");
   });
   $leftArrow.click(function(e) {
     e.preventDefault();
-    $slider.slick("slickPrev");
+    $sliderGoods.slick("slickPrev");
   });
   $rightArrow.click(function(e) {
     e.preventDefault();
-    $slider.slick("slickNext");
+    $sliderGoods.slick("slickNext");
   });
 });
